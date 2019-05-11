5c71ad8e8a983d4c4385fc0c3946d3a6c14011c5
liusihan <31125368+liusihan@users.noreply.github.com>
2019-05-11 11:26:37 +0800

    Update Fine-mapping.sh

--- GWAS/Fine-mapping.sh
+++ GWAS/Fine-mapping.sh
@@ -1,5 +1,5 @@
-cat /zs32/data-analysis/liucy_group/shareData/Chinese_brain/eQTL_result/Chinese_brain_significant.txt | awk '!a[$1]++' | grep -v X | awk '{print $1,$2}' | while read gene chr; do 
-    cat /zs32/data-analysis/liucy_group/shareData/Chinese_brain/eQTL_result/Chinese_brain_significant.txt | awk '$1=="'$gene'"{print $8,$13}' >"$gene".txt; 
+cat Chinese_brain_significant.txt | awk '!a[$1]++' | grep -v X | awk '{print $1,$2}' | while read gene chr; do 
+    cat Chinese_brain_significant.txt | awk '$1=="'$gene'"{print $8,$13}' >"$gene".txt; 
     Rscript Zscore.r "$gene".txt "$gene".Z.txt; 
     cat $gene.Z.txt | sed 's/"//g' >$gene.rs.Z.txt; cat $gene.rs.Z.txt | awk '{print $1}' >eSNP.ID; 
     plink --bfile chr"$chr" --extract eSNP.ID --make-bed --out $gene; 
