2b80dfccc2ecd61b7d0e1695681ee0a99c821da8
liusihan <31125368+liusihan@users.noreply.github.com>
2019-05-11 09:22:57 +0800

    QTL replication

--- pi1.r
+++ pi1.r
@@ -6,6 +6,7 @@ args <- commandArgs(trailingOnly = TRUE)
 try(if(length(args) != 1) stop("Incorrect number of arguments, usage> Rscript pi1.R INPUT"))
 opt_input  = args[1];
 
+ #caucalate pi1
 a<-read.table(opt_input)
 library(qvalue)
 pi0<-pi0est(a$V1,lambda=seq(0.1,0.9,0.1),method="smoother")
