00774891a085f059ae2773274d5060dbd9d3becc
liusihan <31125368+liusihan@users.noreply.github.com>
2019-05-14 10:11:36 +0800

    QTL mapping pipeline

--- QTL_mapping.sh
+++ QTL_mapping.sh
@@ -1,11 +1,24 @@
 ##Sample match check (QTLtools and NGSmakecheck)
 ls RNA-seq/BAM/*Aligned.sortedByCoord.out_Pit_re_marked.bam | awk '{split($1,a,"/");split(a[5],b,".");print b[1]}' | while read line; do QTLtools_1.0_CentOS6.8_x86_64 match --bam RNA-seq/BAM/"$line".sortedByCoord.out_Pit_re_marked.bam --vcf DNA-seq/VCF/Combine/chr22.vcf.gz --filter-mapping-quality 150 --out "$line"; cat "$line" | awk '{print $1"""_"""substr("'$line'",1,length("'$line'")-7),$7/$3,$8/$4}'>"$line".reslut;done
 
+##Sex check
+# remove X chromosome pseudo-autosomal region
+plink --bfile chrall --split-x hg19 --make-bed --out chrall.splitX
 
-##Remove Sex miss match sample(sample ID:PTB041GPTB137G)
+#sexcheck (use default 0.2/0.8 F-statistic thresholds, eyeballing the distribution of F-estimates)
+plink --bfile chrall.splitX --check-sex ycount 0.2 0.8 --out chrall.sexcheck
+
+##Population structure check
+plink --bfile chrall --bmerge KG.bim KG.bed KG.fam --make-bed --out population_structure_check
+plink --bfile population_structure_check --pca 20 --out population_structure_check
+
+##PI_HAT check
+plink --bfile chrall.splitX --genome --out chrall
+
+##Remove Sex miss match sample
 for i in {1..22} X
 do
-vcftools --gzvcf trans_chr"$i".vcf.gz --remove Sex_miss.ID --recode --out chr"$i"_fsample
+vcftools --gzvcf raw_chr"$i".vcf.gz --remove Sex_miss.ID --recode --out chr"$i"_fsample
 done
 
 
@@ -53,18 +66,17 @@ for CHR in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23; do plink
 bgzip -c ${CHR}.vcf >${CHR}.vcf.gz done
 
 
-###Imputation (Michigan)
+###Imputation (Michigan imputation serve: https://imputationserver.sph.umich.edu/index.html)
 
 #wget download the result and Unzip the VCF files using the password provided in the email:
 ls *.zip | xargs -I {} unzip -P 'myPassword' {}
 e.g.: ls *.zip | xargs -I {} unzip -P 'dEIi2b7OQSd@Ia' {}
 
-#Remove snps which R2<0.3 MAF<0.01
+#Remove snps which R2<0.3
 for i in {1..22}
 do 
 zcat chr$i.dose.vcf.gz | awk '{if($1~/^#/){print}else{flag=0;flag2=0;split($8,a,";");for(i in a){if((a[i]~/^R2=/&&substr(a[i],4)>=0.3)){flag=1};if(a[i]~/^MAF=/&&substr(a[i],5)>=0.01){flag2=1}}if(flag==1&&flag2==1){print}}}'>chr$i.michigan.impute.r2.maf.vcf 
 done
-
 
 
 #Remove snps with AF diff >0.1 and hwe<0.000001 maf<0.05
@@ -86,15 +98,12 @@ for i in {1..22}
 do plink --bfile all.impute-updated-chr"$i" --hwe 0.000001 --maf 0.05 --make-bed --out all.impute-updated-chr"$i"_filter
 done
 
-for i in {1..22}
-do plink --bfile all.impute-updated-chr"$i" --hwe 0.000001 --make-bed --out all.impute-updated-chr"$i"_filter
-done
-
-
 for CHR in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22; do plink --bfile all.impute-updated-chr${CHR}_filter --out ${CHR} --recode vcf-fid; done
 
+#generate file contain allele frequncy to assece imputation accuracy
 for i in {1..22}; do cat chr"$i".michigan.impute.r2.maf.vcf | awk 'NR==FNR&&$1!~/#/{split($8,a,";");for(i in a){if(a[i]~/^AF=/){b[$3""":"""$4""":"""$5]=substr(a[i],4)}}}NR>FNR{split($1,a,":");if("'$i'"""":"""a[2]""":"""a[3]""":"""a[4] in b){print $1,b["'$i'"""":"""a[2]""":"""a[3]""":"""a[4]],$8};if("'$i'"""":"""a[2]""":"""a[4]""":"""a[3] in b){print $1,b["'$i'"""":"""a[2]""":"""a[4]""":"""a[3]],1-$8}}'  - <(zcat 1000GP_Phase3_chr"$i".legend.gz) >>Freq_compare_1000G.txt; done
 
+#make genotype files for QTL maaping
 for i in {1..22}; do     awk '          
         BEGIN{FS="\t";OFS="\t"}
         NR==FNR{for(i=2;i<=NF;i++) order[$i]=i+8}
@@ -126,12 +135,11 @@ for i in {1..22}; do     awk '
         }
     ' <(zcat phenotype.chr10.bed.gz  | awk 'NR==1{for(i=5;i<=NF;i++){b=b"""\t"""$i};print b}') <(cat "$i".vcf) | bgzip>genotypes.all.chr$i.vcf.gz; tabix -p vcf genotypes.all.chr$i.vcf.gz; done
 
-##Make bed file
-###Phenotype
-## Convert to bed & locate TSS for reverse strand
+## Make phenotype bed file
+# Convert to bed & locate TSS for reverse strand
 awk 'BEGIN{FS="\t";OFS="\t"}NR==FNR{a[$1]=1}NR>FNR{if($4 in a){start=$3;$3=$2;$2=start}; print $0}' <(zcat gencode.v19.annotation.bed.gz |awk 'BEGIN{FS="[\t.]"}$5=="gene"{a[$7]=$6}END{for(i in a){if(a[i]=="-") print i}}') <(awk 'BEGIN{FS="\t";OFS="\t"}NR==FNR&&$5=="gene"{split($8,s,".");a[s[1]]=$1"\t"$2"\t"$3}NR>FNR&&FNR==1{print "#chr\tstart\tend\tgene"$0}NR>FNR&&FNR>1&&($1 in a){print a[$1],$0}' <(zcat gencode.v19.annotation.bed.gz) log2cpm.fgene.fsample.qn) |sed -e's/^chr//'>log2cpm.fgene.fsample.qn.bed
 
-## Split by chr
+# Split by chr
 for i in {1..22} X
 do
 cat log2cpm.fgene.fsample.qn.bed | awk 'NR==1{print $0}NR>=2&&$1=="'$i'"{print $0}' | sort -k1,1n -k2,2n | awk '{ $4=$4" . +"; print $0 }' | tr " " "\t" | bgzip >phenotype.chr"$i".bed.gz
