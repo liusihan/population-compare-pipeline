222d1e750a69515ba9e5be0048ef02cb029bae06
liusihan <31125368+liusihan@users.noreply.github.com>
2019-12-02 17:09:30 +0800

    Update pLDSC.sh

--- GWAS/pLDSC.sh
+++ GWAS/pLDSC.sh
@@ -1,7 +1,11 @@
-for k in 1000 2000 5000 10000 20000 500000 1000000; do
-  cat Chinese_eQTL.txt | awk '{if($4>$3){print $2,$3-'$k',$4+'$k'}else{print $2,$4-'$k',$3+'$k'}}' >Chinese__eGene_"$k".bed;
-  for i in {1..22}; do cat ~/database/LDSR/1000G_Phase3_EAS_baseline_v1.2_ldscores/"$i".txt | awk 'NR==FNR&&$1=='$i'{a[NR]=$2;b[NR]=$3}NR>FNR&&FNR==1{print "Chinese"}NR>FNR&&FNR>=2{flag=0;for(i in a){if(a[i]<$1&&b[i]>$1){flag=1}};print flag}' Chinese_specific_eGene_"$k".bed - >chinese_annotate_"$i".txt; done;
-  for i in {1..22}; do zcat ~/database/LDSR/1000G_Phase3_EAS_baseline_v1.2_ldscores/baseline."$i".annot.gz | awk 'BEGIN{OFS="\t"}NR==FNR{a[NR]=$0}NR>FNR{print a[FNR],$1}' - chinese_annotate_"$i".txt | gzip >baseline"$k"."$i".annot.gz; done;
-  for i in {1..22}; do python ~/ldsc/ldsc.py --l2 --bfile ~/database/LDSR/1000G_Phase3_EAS_plinkfiles/1000G.EAS.QC."$i" --ld-wind-cm 1 --annot baseline"$k"."$i".annot.gz --print-snps ~/database/LDSR/hapmap3_snps/hm."$i".snp --out baseline"$k"."$i"; done;  
-  python ~/ldsc/ldsc.py --h2 ../PGC.eas.SCZ.sumstats.gz --ref-ld-chr baseline"$k". --w-ld-chr /zs32/home/shliu/database/LDSR/1000G_Phase3_EAS_weights_hm3_no_MHC/weights.EAS.hm3_noMHC. --overlap-annot --frqfile-chr ~/database/LDSR/1000G_Phase3_EAS_plinkfiles/1000G.EAS.QC. --out EAS.SCZ_"$k"; done
+#Add an annotation column(eSNPs or sSNPs) in baseline model
+for i in {1..22}; do zcat ~/database/LDSR/EUR_baseline_2.2/baselineLD."$i".annot.gz | awk 'BEGIN{OFS="\t"}NR==FNR{a[$9""$10]=0}NR>FNR&&FNR==1{print $0,"robust_eQTL"}NR>FNR&&FNR>=2{if($1""$2 in a){print $0,1}else{print $0,0}}' nominal_eQTL.txt - | gzip >~/database/LDSR/my_baseline_EUR/baselineLD."$i".annot.gz; done
 
+#Calculated LD score
+for i in {1..22}; do python ~/ldsc/ldsc.py --l2 --bfile ~/database/LDSR/1000G_EUR_Phase3_plink/1000G.EUR.QC."$i" --ld-wind-cm 1 --annot ~/database/LDSR/my_baseline_EUR/baselineLD."$i".annot.gz --print-snps ~/database/LDSR/hapmap3_snps/hm."$i".snp --out ~/database/LDSR/my_baseline_EUR/baselineLD."$i"; done
+
+#Make sumstats file
+python ~/ldsc/munge_sumstats.py --sumstats PGC3.SCZ.summary --N 65967 --out PGC3.SCZ --merge-alleles ~/database/LDSR/w_hm3.snplist --a1-inc
+
+#Partitioned heritability
+python ~/ldsc/ldsc.py --h2 PGC3.SCZ.sumstats.gz --ref-ld-chr ~/database/LDSR/my_baseline_EUR/baselineLD. --w-ld-chr /zs32/home/shliu/database/LDSR/weights_hm3_no_hla/weights. --overlap-annot --frqfile-chr ~/database/LDSR/1000G_Phase3_frq/1000G.EUR.QC. --out PGC_SCZ
